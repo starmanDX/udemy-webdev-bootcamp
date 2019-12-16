57a1a0744c938f1487733341ab4178724ba8feee
starmanDX <58798270+starmanDX@users.noreply.github.com>
2019-12-16 13:41:12 -0800

    Add js-stalker files via upload
    Added HTML and JS files for JS Stalker exercise

---
+++ js-stalker/scripts/main.js
@@ -0,0 +1,7 @@
+let firstName = prompt("What is your First Name");
+let lastName = prompt("What is your last name?");
+let age = prompt("What is your age?");
+alert("Your full name is " + firstName + " " + lastName);
+alert("Your age is " + age);
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("Your age is " + age);
