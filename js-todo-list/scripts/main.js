7ab75781e7cbdbff2821d9cabe27cace273fb09a
starmanDX <58798270+starmanDX@users.noreply.github.com>
2019-12-20 00:05:42 -0800

    Update main.js
    Add comments

--- js-todo-list/scripts/main.js
+++ js-todo-list/scripts/main.js
@@ -1,11 +1,11 @@
 let toDoList = [];
-let userAction = prompt("What would you like to do");
 
-window.setTimeout(function () {
-    while (userAction !== "quit") {
-        if (userAction === "list") {
+window.setTimeout(function () {  //allows the HTML to load before the prompt
+    let userAction = prompt("What would you like to do");
+    while (userAction !== "quit") { //reprompts the user until "quit" is selected
+        if (userAction === "list") { //shows the toDoList array
             console.log(toDoList);
-        } else if (userAction === "new") {
+        } else if (userAction === "new") { //asks user for new todo item, and adds it to the toDoList array
             let newToDo = prompt("Add a Todo item");
             toDoList.push(newToDo);
 
