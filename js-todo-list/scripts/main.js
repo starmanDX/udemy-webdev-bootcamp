2c48c837ec98ce858932c1cf8317c3e05ae49870
starmanDX <58798270+starmanDX@users.noreply.github.com>
2019-12-20 16:29:54 -0800

    Update main.js
    Updated main.js for V2

--- js-todo-list/scripts/main.js
+++ js-todo-list/scripts/main.js
@@ -1,16 +1,36 @@
 let toDoList = [];
 
-window.setTimeout(function () {  //allows the HTML to load before the prompt
+function listTodos() { //shows current list
+    console.log("**********");
+    toDoList.forEach(function (todo, i) {
+        console.log(i + ": " + todo);
+    });
+    console.log("**********");
+}
+
+function newTodo() { //adds new todo to list
+    let newToDo = prompt("Add a Todo item");
+    toDoList.push(newToDo);
+    console.log(newToDo + " added to list");
+}
+
+function deleteTodo() { //deletes todo from list
+    let deleteIndex = prompt("Which index do you want to delete");
+    let deleteName = toDoList.splice(deleteIndex, 1);
+    console.log(deleteName + " removed from list");
+}
+
+window.setTimeout(function () { //allows the HTML instructions to load before the prompt
     let userAction = prompt("What would you like to do");
     while (userAction !== "quit") { //reprompts the user until "quit" is selected
-        if (userAction === "list") { //shows the toDoList array
-            console.log(toDoList);
-        } else if (userAction === "new") { //asks user for new todo item, and adds it to the toDoList array
-            let newToDo = prompt("Add a Todo item");
-            toDoList.push(newToDo);
-
+        if (userAction === "list") { //calls listTodos function
+            listTodos();
+        } else if (userAction === "new") { //calls newTodo function
+            newTodo();
+        } else if (userAction === "delete") { //calls deleteTodo function
+            deleteTodo();
         }
         userAction = prompt("What would you like to do");
     }
-    console.log("OK, You quit the app.");
+    console.log("OK, you quit the app.");
 }, 500);
