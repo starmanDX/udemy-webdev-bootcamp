64c5375e5d3320d12730506cd4a699f6d7e76e32
starmanDX <jamiecordrey@gmail.com>
2020-01-07 01:12:25 -0800

    Add YelpCampV11 files via upload
    Added files for YelpCampV11 project via upload

---
+++ YelpCamp/v11/routes/campgrounds.js
@@ -0,0 +1,106 @@
+const express   = require("express"),
+    campground  = require("../models/campground"),
+    middleware  = require("../middleware"),
+    router      = express.Router({
+        mergeParams: true
+    });
+
+// INDEX ROUTE - show all campgrounds
+router.get("/", (req, res) => {
+    campground.find({}, (err, allCampgrounds) => {
+        if (err) {
+            req.flash("error", "Something went wrong.");
+            res.redirect("back");
+        } else {
+            res.render("campgrounds/index", {
+                campgrounds: allCampgrounds,
+                currentUser: req.user
+            });
+        }
+    })
+});
+
+//NEW ROUTE - show form to create new campground
+router.get("/new", middleware.isLoggedIn, (req, res) => {
+    res.render("campgrounds/new");
+});
+
+//CREATE ROUTE - add new campground to database
+router.post("/", middleware.isLoggedIn, (req, res) => {
+    let name = req.body.name;
+    let image = req.body.image;
+    let description = req.body.description;
+    let author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    let newCampground = {
+        name: name,
+        image: image,
+        description: description,
+        author: author
+    };
+    campground.create(newCampground, (err, newlyCreated) => {
+        if (err) {
+            req.flash("error", "Something went wrong.");
+            res.redirect("back");
+        } else {
+            req.flash("success", "Successfully added campground.");
+            res.redirect("/campgrounds");
+        }
+    })
+});
+
+//SHOW ROUTE - shows info about one campground
+router.get("/:id", (req, res) => {
+    campground.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
+        if (err || !foundCampground) {
+            req.flash("error", "Campground not found.");
+            res.redirect("/campgrounds");
+        } else {
+            res.render("campgrounds/show", {
+                campground: foundCampground
+            });
+        }
+    });
+})
+
+//EDIT ROUTE
+router.get("/:id/edit", middleware.checkCampgroundOwnership, (req,res) => {
+    campground.findById(req.params.id, (err, foundCampground) => {
+        if(err || !foundCampground) {
+            req.flash("error", "Something went wrong.");
+            res.redirect("back");
+        } else {
+            res.render("campgrounds/edit", {campground: foundCampground});
+        }
+    });
+});
+
+//UPDATE ROUTE
+router.put("/:id", middleware.checkCampgroundOwnership, (req,res) => {
+    campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCampground) => {
+        if(err) {
+            req.flash("error", "Something went wrong.");
+            res.redirect("back");
+        } else {
+            req.flash("success", "Successfully updated campground.");
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    })
+});
+
+//DESTROY ROUTE
+router.delete("/:id", middleware.checkCampgroundOwnership, (req,res) => {
+    campground.findByIdAndRemove(req.params.id, (err) => {
+        if(err) {
+            req.flash("error", "Something went wrong.");
+            res.redirect("back");
+        } else {
+            req.flash("success", "Successfully deleted campground.");
+            res.redirect("/campgrounds");
+        }
+    })
+});
+
+module.exports = router;
